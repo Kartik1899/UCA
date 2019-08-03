0a5f76824d747be49010c5317a3e9b21bd09f239
Kartik1899 <ranakartik16@gmail.com>
2019-08-03 10:02:12 +0530

    Without using transpose

---
+++ RotateArray90b.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+
+// rotating array 90 degree without taking transpose
+
+using namespace std;
+
+
+void rotateArray(int **arr, int size)                               //rotate function
+{
+
+    for(int i = 0 ; i< size/2 ;i++)
+	{
+
+		for(int j = i ; j < size-1-i; j++)
+		{
+			int temp = arr[ i ][ j ];
+
+			arr[ i ][ j ] = arr[ size-1-j ][ i ];
+			
+            arr[ size-1-j ][ i ] = arr[ size-1-i ][ size-1-j ];
+			
+            arr[ size-1-i ][ size-1-j ] = arr[ j ][ size-1-i ];
+			
+            arr[ j ][ size-1-i ] = temp;	
+		}
+
+	}
+}
+
+int main()
+{
+    int size;
+    cin >> size;
+
+
+    int **arr = new int*[size];                 // initializing 2d array
+              
+    for(int i = 0 ; i < size ;i++)
+    {
+        arr[i] = new int[size];
+    }
+
+    for(int i = 0 ; i < size ; i++)
+    {
+        for(int j = 0 ; j < size ; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+
+    rotateArray(arr,size);                      // calling function()
+
+
+    for(int i = 0 ; i < size ; i++)
+    {
+        for(int j = 0 ; j < size ; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
